6bd871c565f6e8925b12f15f7551892b9b41a738
brnunez15 <brnunez@uade.edu.ar>
2025-09-18 18:22:59 -0300

    estructura en proceso

---
+++ cargar_compra.py
@@ -0,0 +1,2 @@
+def opciones():
+    pass
---
+++ main.py
@@ -0,0 +1,52 @@
+from tabulate import tabulate
+from cargar_compra import agregar_producto
+
+
+def mostrar_opciones():
+    opciones = [
+        ["1", "Cargar Compra"],
+        ["2", "Ver Compras"],
+        ["3", "Productos"],
+        ["4", "Descuentos"],
+        ["5", "Cierre de Caja"],
+        ["0", "Salir"]
+    ]
+        
+    return (tabulate(opciones, headers=["Opción", "Descripción"], tablefmt='fancy_grid', colalign=("center", "left")))
+
+def menu():
+
+    while True:
+
+        opciones = mostrar_opciones()
+        print(opciones)
+
+        op = input("Ingrese una opcion: ")
+
+        if op == "0":
+            print("Saliendo...")
+            break
+
+        elif op == "1":
+            pass
+
+        elif op == "2":
+            pass
+
+        elif op == "3":
+            pass
+
+        elif op == "4":
+            pass
+
+        elif op == "5":
+            pass
+
+        else:
+            print("Opcion invalida")
+
+def main():
+    menu()
+
+if __name__ == "__main__":
+    main()
