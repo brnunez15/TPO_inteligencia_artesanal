3cc1074bfb8be3b24efb7f5d225956000dfcf1ae
nalescano <nataliajosefinalescano@gmail.com>
2025-09-18 19:42:22 -0300

    creación del modulo de productos
    - creación del menu que llama a las funciones.
    - creación de funciones.

---
+++ PRODUCTOS/productos.py
@@ -0,0 +1,80 @@
+from tabulate import tabulate 
+import actualizar_producto
+
+
+def ver_producto():
+    '''
+    muestra un diccionario con todos los datos de los productos actuales.
+    '''
+    pass
+
+
+def agregar_producto():
+    '''
+    permite agregar un producto nuevo al diccionario de productos
+    '''
+    pass
+
+
+def opciones_productos():
+    '''
+    muestra las opciones de la pestaña de productos.
+
+    Pre:
+        - no ingresa ningun valor como parametro.
+
+    Post:
+        - devuelve un string con una tabla ya formada.
+    '''
+    
+    opciones = [
+        ['1', 'Ver productos'],
+        ['2', 'Agregar productos'],
+        ['3', 'Actualizar Producto'],
+        ['0', 'Salir']
+    ]
+
+    return tabulate(
+        opciones, 
+        headers=["Opción", "Descripción"], 
+        tablefmt='fancy_grid', 
+        colalign=("center", "left")
+        )
+
+
+def menu_productos():
+    '''
+    compara la opción que ingreso el usuario con las opciones del menu y llama a las funciones.
+    '''
+
+    while True:
+        opciones = opciones_productos()
+        print(opciones)
+
+        op = input('ingrese una opcion: ')
+
+        if op == '0':
+            break
+
+        elif op == '1':
+            ver_producto()
+
+        elif op == '2':
+            agregar_producto()
+
+        elif op == '3':
+            actualizar_producto()
+
+        else: 
+            print('opcion incorrecta')
+
+
+def main():
+    '''
+    funcion principal del modulo, llama al menu del producto.
+    '''
+    menu_productos()
+
+
+if __name__ == '__main__':
+    main()
