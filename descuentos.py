2d08fed8c67125727e4b70ff47021365eca3bd11
Seuz1111 <sebacarini10@gmail.com>
2025-10-17 00:22:07 -0300

    json y funcion nueva

--- descuentos.py
+++ descuentos.py
@@ -1,39 +1,54 @@
 from tabulate import tabulate
+import json
 
 def ver_descuentos():
     """ Muestra todos los descuentos disponibles
-
+ 
         pre: Ninguna
-
+ 
         post: Muestra una lista con los descuentos disponibles
-
     """
-    pass
+    with open("descuentos.json") as contenido:
+            descuentos = json.load(contenido)
 
-def agregar_descuento(porcentaje: int, descripcion: str):
-    """ Agrega un nuevo descuento
-    
-        pre: porcentaje es un numero entre 0 y 100 y descripcion representa el medio de pago por el cual se aplica el descuento
-    
-        post: Agrega el descuento a la base de datos
-    """
-    pass
+    lista_descuentos = descuentos["descuentos_semanales"]
 
-def eliminar_descuento(descuento_id: int):
-    """ Elimina un descuento existente
+    for descuento in lista_descuentos:
+        print(f"{descuento["nombre_dia"]}, {descuento["descuento"]}, {descuento["metodo_de_pago"]}")
+
+def modificar_descuento(dia: str, nuevo_descuento: int, nuevo_metodo_de_pago: str):
+    """ Modifica un descuento existente
     
-        pre: descuento_id entero que representa el id del descuento a eliminar
+        pre: dia es un string que representa el dia de la semana, nuevo_descuento es un numero entre 0 y 100 y nuevo_metodo_de_pago representa el medio de pago por el cual se aplica el descuento
     
-    
-        post: Elimina el descuento del archivo 
+        post: Modifica el descuento en la base de datos
     """
-    pass
+    with open("descuentos.json", "r", encoding='utf-8') as contenido:
+            descuentos = json.load(contenido)
+
+    lista_descuentos = descuentos["descuentos_semanales"]
+
+    dia_encontrado = False
+    
+    for descuento in lista_descuentos:
+        if descuento["nombre_dia"].lower() == dia.lower():
+            descuento["descuento"] = nuevo_descuento
+            descuento["metodo_de_pago"] = nuevo_metodo_de_pago
+            dia_encontrado = True
+            print(f"Datos para {dia} actualizados")
+            break
+
+    if dia_encontrado == True:
+        with open("descuentos.json", "w", encoding='utf-8') as contenido:
+            json.dump(descuentos, contenido, indent=4, ensure_ascii=False)
+        print(f"Archivo '{"descuentos.json"}' guardado con éxito")
+    else:
+        print(f"No se encontró el día {dia} El archivo no fue modificado")
 
 def mostrar_opciones():
     opciones = [
         ["1", "Ver Descuentos"],
-        ["2", "Agregar Descuento"],
-        ["3", "Eliminar Descuento"],
+        ["2", "Modificar Descuento"],
         ["0", "Salir"]
     ]
         
@@ -52,17 +67,17 @@ def menu():
             print("Saliendo...")
             break
 
+
         elif op == "1":
             ver_descuentos()
-            pass
+            
 
         elif op == "2":
-            agregar_descuento()
-            pass
-
-        elif op == "3":
-            eliminar_descuento()
-            pass
+            dia = input("Ingrese el dia a modificar: ")
+            nuevo_descuento = int(input("Ingrese el nuevo descuento (entre 0 y 100): "))
+            nuevo_metodo_de_pago = input("Ingrese el nuevo metodo de pago: ")
+            modificar_descuento(dia, nuevo_descuento, nuevo_metodo_de_pago)
+            
 
         else:
             print("Opcion invalida")
